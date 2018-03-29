05280110bde37a7d5012f36dacab17c9a7fa8738
robert <yusheng.chen@sunplus.com>
2018-03-29 16:13:16 +0800

    [Toolchain][ARMv7] Add toolchain for ARMv7hf build [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] Please find prebuilt and its source from https://toolchains.bootlin.com/ [Test   Case] Boot ca7/ca9 linux on 8388/i136 ok [Compile] OK [PCLINT] N/A [Selftest] OK [GPL-licensed & Third-party Source] N/A

---
+++ tools/armv7-eabihf--glibc--stable/arm-buildroot-linux-gnueabihf/sysroot/usr/include/linux/version.h
@@ -0,0 +1,2 @@
+#define LINUX_VERSION_CODE 199273
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
---
+++ tools/armv7-eabihf--glibc--stable/lib/gcc/arm-buildroot-linux-gnueabihf/5.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/tmp/armv7-eabihf--glibc--stable/usr --sysconfdir=/tmp/armv7-eabihf--glibc--stable/etc --enable-static --target=arm-buildroot-linux-gnueabihf --with-sysroot=/tmp/armv7-eabihf--glibc--stable/usr/arm-buildroot-linux-gnueabihf/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/tmp/armv7-eabihf--glibc--stable/usr --with-mpc=/tmp/armv7-eabihf--glibc--stable/usr --with-mpfr=/tmp/armv7-eabihf--glibc--stable/usr --with-pkgversion='Buildroot 2017.05' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --disable-decimal-float --with-abi=aapcs-linux --with-cpu=cortex-a9 --with-fpu=vfpv3-d16 --with-float=hard --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/tmp/armv7-eabihf--glibc--stable/usr/arm-buildroot-linux-gnueabihf/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a9" }, { "float", "hard" }, { "mode", "arm" }, { "fpu", "vfpv3-d16" }, { "tls", "gnu" } };
