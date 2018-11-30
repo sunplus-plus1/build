71360e72c814def300cfade801504fe6dfbb6188
henry.liou <henry.liou@sunplus.com>
2018-11-30 14:26:24 +0800

    [Config] Modified config.sh. [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc]     1.Add nand config for A/B chip in config.sh [Test   Case] Compile pass. [Compile] OK [Coverity] N/A [Selftest] OK [GPL-licensed & Third-party Source] N/A

--- config.sh
+++ config.sh
@@ -10,6 +10,19 @@ XBOOT_CONFIG_ROOT=./boot/xboot/configs
 UBOOT_CONFIG_ROOT=./boot/uboot/configs
 KERNEL_CONFIG_ROOT=./linux/kernel/arch/arm/configs
 
+pentagram_b_chip_nand_config()
+{
+	XBOOT_CONFIG=q628_defconfig
+	UBOOT_CONFIG=pentagram_sc7021_nand_b_defconfig
+	KERNEL_CONFIG=pentagram_sc7021_bchip_emu_defconfig
+	CROSS_COMPILE=$1
+	NEED_ISP=1
+	echo "XBOOT_CONFIG=${XBOOT_CONFIG}" > $BUILD_CONFIG
+	echo "UBOOT_CONFIG=${UBOOT_CONFIG}" >> $BUILD_CONFIG
+	echo "KERNEL_CONFIG=${KERNEL_CONFIG}" >> $BUILD_CONFIG
+	echo "CROSS_COMPILE="$CROSS_COMPILE >> $BUILD_CONFIG
+	echo "NEED_ISP="$NEED_ISP >> $BUILD_CONFIG
+}
 pentagram_b_chip_emmc_config()
 {
 	XBOOT_CONFIG=q628_defconfig
@@ -33,6 +46,19 @@ pentagram_b_chip_nor_config()
 	echo "UBOOT_CONFIG=${UBOOT_CONFIG}" >> $BUILD_CONFIG
 	echo "KERNEL_CONFIG=${KERNEL_CONFIG}" >> $BUILD_CONFIG
 	echo "CROSS_COMPILE="$CROSS_COMPILE >> $BUILD_CONFIG
+}
+pentagram_a_chip_nand_config()
+{
+	XBOOT_CONFIG=q628_defconfig
+	UBOOT_CONFIG=pentagram_sc7021_nand_defconfig
+	KERNEL_CONFIG=pentagram_sc7021_achip_emu_defconfig
+	CROSS_COMPILE=$1
+	NEED_ISP=1
+	echo "XBOOT_CONFIG=${XBOOT_CONFIG}" > $BUILD_CONFIG
+	echo "UBOOT_CONFIG=${UBOOT_CONFIG}" >> $BUILD_CONFIG
+	echo "KERNEL_CONFIG=${KERNEL_CONFIG}" >> $BUILD_CONFIG
+	echo "CROSS_COMPILE="$CROSS_COMPILE >> $BUILD_CONFIG
+	echo "NEED_ISP="$NEED_ISP >> $BUILD_CONFIG
 }
 pentagram_a_chip_emmc_config()
 {
@@ -154,11 +180,13 @@ others_config()
 
 $ECHO $COLOR_GREEN"Q628 configs."$COLOR_ORIGIN
 $ECHO $COLOR_YELLOW"[1] Pentagram B chip (EMMC)"$COLOR_ORIGIN
-$ECHO $COLOR_YELLOW"[2] Pentagram B chip (NOR/romter)"$COLOR_ORIGIN
-$ECHO $COLOR_YELLOW"[3] Pentagram A chip (EMMC)"$COLOR_ORIGIN
-$ECHO $COLOR_YELLOW"[4] Pentagram A chip (NOR/romter)"$COLOR_ORIGIN
-$ECHO $COLOR_YELLOW"[5] 8388 B chip"$COLOR_ORIGIN
-$ECHO $COLOR_YELLOW"[6] others"$COLOR_ORIGIN
+$ECHO $COLOR_YELLOW"[2] Pentagram B chip (SPI-NAND)"$COLOR_ORIGIN
+$ECHO $COLOR_YELLOW"[3] Pentagram B chip (NOR/romter)"$COLOR_ORIGIN
+$ECHO $COLOR_YELLOW"[4] Pentagram A chip (EMMC)"$COLOR_ORIGIN
+$ECHO $COLOR_YELLOW"[5] Pentagram A chip (SPI-NAND)"$COLOR_ORIGIN
+$ECHO $COLOR_YELLOW"[6] Pentagram A chip (NOR/romter)"$COLOR_ORIGIN
+$ECHO $COLOR_YELLOW"[7] 8388 B chip"$COLOR_ORIGIN
+$ECHO $COLOR_YELLOW"[8] others"$COLOR_ORIGIN
 read num
 
 case "$num" in
@@ -166,18 +194,24 @@ case "$num" in
 		pentagram_b_chip_emmc_config $1
 		;;
 	2)
-		pentagram_b_chip_nor_config $1
+		pentagram_b_chip_nand_config $1
 		;;
 	3)
-		pentagram_a_chip_emmc_config $2
+		pentagram_b_chip_nor_config $1
 		;;
 	4)
-		pentagram_a_chip_nor_config $2
+		pentagram_a_chip_emmc_config $2
 		;;
 	5)
-		pentagram_8388_b_chip_config $1
+		pentagram_a_chip_nand_config $2
 		;;
 	6)
+		pentagram_a_chip_nor_config $2
+		;;
+	7)
+		pentagram_8388_b_chip_config $1
+		;;
+	8)
 		others_config $1 $2
 		;;
 	*)
